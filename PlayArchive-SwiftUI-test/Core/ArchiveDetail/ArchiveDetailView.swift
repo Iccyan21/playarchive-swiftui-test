fbc58fb618f575f3b0077d5f2a233f332055e6fa
Iccyan21 <iccyan0429@gmail.com>
2024-01-03 18:14:16 +0900

    Detail追加

---
+++ PlayArchive-SwiftUI-test/Core/ArchiveDetail/ArchiveDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArchiveDetailView.swift
+//  PlayArchive-SwiftUI-test
+//
+//  Created by 水原　樹 on 2024/01/03.
+//
+
+import SwiftUI
+
+struct ArchiveDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArchiveDetailView()
+}
